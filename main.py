22fedc16a005452a07c863284e7146776018244a
ladailam382 <ladailam382@gmail.com>
2023-02-02 00:00:34 +0700

    update deployemnt

---
+++ main.py
@@ -0,0 +1,33 @@
+# Importing Necessary modules
+from src.app.core.utils.utils import *
+from src.app.core.api import api
+# from colabcode import ColabCode
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
+
+
+
+# Declaring our FastAPI instance
+app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="src/app/templates/static"), name="static")
+# app.mount("/staging", StaticFiles(directory="src/app/staging"), name="staging")
+
+
+# @app.on_event('startup')
+# async def download_model_wandb():
+#     download_model()
+
+# @app.on_event("shutdown")
+# def shutdown_event():
+#     files = glob.glob('./app/staging/video/*.mp4')
+#     for file in files:
+#         print(file)
+#         os.remove(file)
+
+
+app.include_router(api.router)
+
+# cc = ColabCode(port=12000, code=False)
+# cc.run_app(app=app)
