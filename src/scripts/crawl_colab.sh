249a359bccd3981e60cd680cf9f1c5e821d5d7b5
ladailam382 <ladailam382@gmail.com>
2023-02-01 12:02:00 +0700

    update job id

--- src/scripts/crawl_colab.sh
+++ src/scripts/crawl_colab.sh
@@ -4,8 +4,8 @@ end_page=1
 
 for i in vieclamtot vieclam24h
 do
-    echo /content/DS.20221.04.JobPostClassifications/data_pipeline/extract/"$i"_scraper/
-    cd /content/DS.20221.04.JobPostClassifications/data_pipeline/extract/"$i"_scraper/
-    scrapy crawl $i -a end_page="$end_page"
+    echo /content/DS.20221.04.JobPostClassifications/src/data_pipeline/extract/"$i"_scraper/
+    cd /content/DS.20221.04.JobPostClassifications/src/data_pipeline/extract/"$i"_scraper/
+    scrapy crawl $i
     echo "Finished crawling $i"
 done
